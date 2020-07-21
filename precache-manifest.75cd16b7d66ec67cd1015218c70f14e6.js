9d10dc62bfbcf83a54cb3ba34f55c7d143efb49e
username396 <cuozlokchok@gmail.com>
2020-07-21 23:41:47 +0700

    Updates

---
+++ precache-manifest.75cd16b7d66ec67cd1015218c70f14e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "811c61584150ffdda19aafa10ed4423c",
+    "url": "/react_call_api/index.html"
+  },
+  {
+    "revision": "6b19ac429ee4f01da87f",
+    "url": "/react_call_api/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "bdbaf6772ee108d54ee2",
+    "url": "/react_call_api/static/js/2.756cc125.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_call_api/static/js/2.756cc125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b19ac429ee4f01da87f",
+    "url": "/react_call_api/static/js/main.1fb07a21.chunk.js"
+  },
+  {
+    "revision": "fd413fe96cda644480d3",
+    "url": "/react_call_api/static/js/runtime-main.bba1cb56.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react_call_api/static/media/logo.ee7cd8ed.svg"
+  }
+]);
